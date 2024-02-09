206cd15a3f6e1b76d919e5ac45e87f3ada721ee3
krishnavamshi933 <109383860+krishnavamshi933@users.noreply.github.com>
2024-02-09 13:37:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-// app.js
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, World!\n');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World this is github action  \n');
 });
 
-const PORT = 3000;
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0'; // Bind to all available network interfaces
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+server.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}/`);
 });
